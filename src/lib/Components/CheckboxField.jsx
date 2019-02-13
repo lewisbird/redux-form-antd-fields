7a9e0f0f1cf10596a00e2e0b92536a84636f611d
Lewis Bird <lewis@sotamedia.co.uk>
2019-02-13 16:52:44 +0800

    More checks on Checkbox

--- src/lib/Components/CheckboxField.jsx
+++ src/lib/Components/CheckboxField.jsx
@@ -29,7 +29,10 @@ const mapProps = createMapProps((mappedProps, originalProps) => {
     value: checked
   } = mappedProps;
   
-  const onChange = (event) => _onChange(event.target.value);
+  const onChange = (event, xx) => {
+    console.log(event.target, xx)
+    return _onChange(event.target.value);
+  }
   
   return { ...mappedProps, onChange, checked };
 
